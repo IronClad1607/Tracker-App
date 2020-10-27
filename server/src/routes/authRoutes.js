694631613df53a0a986656521e303fb15f2056fb
Ishaan Jain <ishaan160799@gmail.com>
2020-10-27 18:38:05 +0530

    body parser and request body added

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const { model } = require("mongoose");
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
+  console.log(req.body);
   res.send("You made a post request");
 });
 
