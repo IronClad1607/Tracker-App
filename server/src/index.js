b6dfb7803594862938891c1313c616b32d9d70fa
Ishaan Jain <ishaan160799@gmail.com>
2020-10-27 17:46:59 +0530

    mongo db connection done

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://abc:abc@cluster0.iml6k.mongodb.net/<dbname>?retryWrites=true&w=majorit";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo db");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error".err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi There!");
